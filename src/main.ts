0f154086738943d30d5fb875d9d2e851de2d9793
muhammadyahyo <muhammadyahyodev@gmail.com>
2023-03-20 11:06:42 +0500

    Project started

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
+async function start() {
+  const PORT = process.env.PORT || 7070;
+
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  // app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Nestjs-Vue')
+    .setDescription('REST API')
+    .setVersion('1.0.0')
+    .addTag('NodeJS, NestJS, Vue, Postgres, sequelize')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => {
+    console.log(`\nServer has been started on ${PORT} port...`);
+  });
+}
+
+start();
