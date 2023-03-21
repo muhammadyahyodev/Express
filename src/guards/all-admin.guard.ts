63ed698abef054e0f421d6bc7a72578fe81ccb75
muhammadyahyo <muhammadyahyodev@gmail.com>
2023-03-21 10:54:23 +0500

    create admin guards

---
+++ src/guards/all-admin.guard.ts
@@ -0,0 +1,40 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class CreatorAdminGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+
+    const bearer = authHeader.split(' ')[0];
+    const token = authHeader.split(' ')[1];
+
+    if (bearer !== 'Bearer' || !token) {
+      throw new UnauthorizedException({
+        message: 'Nomzod ro`yhatdan o`tmagan',
+      });
+    }
+
+    const condidate = this.jwtService.verify(token, {
+      publicKey: process.env.ACCESS_TOKEN_KEY,
+    });
+
+    if (!condidate.is_active) {
+      throw new ForbiddenException(`Ruxsat berilmadi`);
+    }
+
+    return true;
+  }
+}
